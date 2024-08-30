71bb46de97cbc7431896d14d1227e8ae7c2acc9c
Martin Borgén <martinb.nymail@gmail.com>
2024-08-30 19:12:07 +0200

    removed clause causing exit if window resized

--- src/main.rs
+++ src/main.rs
@@ -36,8 +36,6 @@ impl GameApp {
                 {
                     self.board.advance_state();
                 }
-            } else {
-                self.exit();
             }
         }
     }
